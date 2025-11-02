3616eb5b5ab9b12d5bbfdc67eb0218805acdbd4f
saurav266 <saurav840963@gmail.com>
2025-11-02 18:09:05 +0530

    s

--- model/userModel.js
+++ model/userModel.js
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    },
-    password: {
+  },
+  password: {
     type: String,
     required: true,
   },    
